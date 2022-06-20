7c21f79026e0bdf7273430a72736ad27a564f3f6
WhaeaSuzanne <101528367+WhaeaSuzanne@users.noreply.github.com>
2022-06-21 09:25:40 +1200

    skeleton of the base code

---
+++ 00_base_v0.py
@@ -0,0 +1,18 @@
+"""
+base component for the game Lucky Unicorn
+V0 - skeleton setup
+
+author- Whaea Suzanne
+CC WSA 2022
+
+"""
+
+# import libraries below this line **************************
+
+
+# all functions below this line **************************
+
+
+
+# main routine below this line **************************
+print("Hello")
