8826fe8a500b58d7c4ee8d42c8a948cc10cae880
WhaeaSuzanne <101528367+WhaeaSuzanne@users.noreply.github.com>
2022-06-30 12:14:03 +1200

    v1 instructions
    not added to the base component yet
    more rules to be added

---
+++ 02_instructions_v1.py
@@ -0,0 +1,16 @@
+"""
+function to print instructions to the screen
+v1 - set up the function
+
+"""
+
+def instructions ():
+    # rules will be added as they are required
+    print("************   How to play   ************")
+    print()
+    print("***  The rules of the game go here.  ***")
+    print()
+    return ""
+
+# main routine
+instructions() #calling line to the instructions function
